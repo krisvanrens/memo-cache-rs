8b4df4278aa0cb8d6fc49dd01d2c88149549c7e4
Kris van Rens <krisvanrens@gmail.com>
2024-10-16 23:06:41 +0200

    CHG: Extend benchmarks with more tests.

--- benches/memo_cache.rs
+++ benches/memo_cache.rs
@@ -1,8 +1,23 @@
 use criterion::{criterion_group, criterion_main, Criterion};
 use memo_cache::MemoCache;
-use rand::Rng;
+use rand::{Rng, SeedableRng};
+use rand_chacha::ChaCha8Rng;
 use rand_distr::{Distribution, Normal};
-use std::{collections::HashMap, thread, time};
+use std::{collections::HashMap, ops::RangeInclusive, thread, time};
+
+// Pseudo random number generator seed value (used for all benches).
+const RNG_SEED_VALUE: u64 = 42;
+
+// Uniform input data distribution settings.
+const UNIFORM_RANGE_NARROW: RangeInclusive<i32> = -30..=30;
+const UNIFORM_RANGE_WIDE: RangeInclusive<i32> = -100..=100;
+
+// Normal input data distribution settings.
+const NORMAL_VARIANCE_NARROW: f32 = 5.0;
+const NORMAL_VARIANCE_WIDE: f32 = 30.0;
+
+// Fixed-size cache capacity for all benches.
+const MEMO_CACHE_CAPACITY: usize = 64; // [elements].
 
 /// Calculate cache size given a capacity (assuming u32 -> u32 caches).
 fn cache_size(capacity: usize) -> usize {
@@ -15,11 +30,13 @@ fn fake_expensive_calculation() {
 
 // Hash map cache, uniform distribution.
 fn bench_hash_map_uniform(c: &mut Criterion) {
-    let mut rng = rand::thread_rng();
+    let mut g = c.benchmark_group("HashMap - Uniform distribution");
+
     let mut cache = HashMap::new();
-    c.bench_function("HashMap (control, uniform)", |b| {
+    let mut rng = ChaCha8Rng::seed_from_u64(RNG_SEED_VALUE);
+    g.bench_function("narrow range", |b| {
         b.iter_batched(
-            || rng.gen_range(-100..=100),
+            || rng.gen_range(UNIFORM_RANGE_NARROW),
             |input| {
                 if cache.get(&input).is_none() {
                     fake_expensive_calculation();
@@ -31,14 +48,79 @@ fn bench_hash_map_uniform(c: &mut Criterion) {
     });
 
     println!("Used cache size: {} bytes", cache_size(cache.capacity()));
+
+    let mut cache = HashMap::new();
+    let mut rng = ChaCha8Rng::seed_from_u64(RNG_SEED_VALUE);
+    g.bench_function("wide range", |b| {
+        b.iter_batched(
+            || rng.gen_range(UNIFORM_RANGE_WIDE),
+            |input| {
+                if cache.get(&input).is_none() {
+                    fake_expensive_calculation();
+                    cache.insert(input, 42);
+                }
+            },
+            criterion::BatchSize::SmallInput,
+        )
+    });
+
+    println!("Used cache size: {} bytes", cache_size(cache.capacity()));
+
+    g.finish();
+}
+
+// Fixed-size cache, uniform distribution.
+fn bench_memo_cache_uniform(c: &mut Criterion) {
+    let mut g = c.benchmark_group(format!(
+        "MemoCache (size: {}) - Uniform distribution",
+        MEMO_CACHE_CAPACITY
+    ));
+
+    let mut cache = MemoCache::<_, _, MEMO_CACHE_CAPACITY>::new();
+    let mut rng = ChaCha8Rng::seed_from_u64(RNG_SEED_VALUE);
+    g.bench_function("narrow range", |b| {
+        b.iter_batched(
+            || rng.gen_range(-10..=10),
+            |input| {
+                cache.get_or_insert_with(&input, |_| {
+                    fake_expensive_calculation();
+                    42
+                });
+            },
+            criterion::BatchSize::SmallInput,
+        )
+    });
+
+    println!("Used cache size: {} bytes", cache_size(cache.capacity()));
+
+    let mut cache = MemoCache::<_, _, MEMO_CACHE_CAPACITY>::new();
+    let mut rng = ChaCha8Rng::seed_from_u64(RNG_SEED_VALUE);
+    g.bench_function("wide range", |b| {
+        b.iter_batched(
+            || rng.gen_range(-100..=100),
+            |input| {
+                cache.get_or_insert_with(&input, |_| {
+                    fake_expensive_calculation();
+                    42
+                });
+            },
+            criterion::BatchSize::SmallInput,
+        )
+    });
+
+    println!("Used cache size: {} bytes", cache_size(cache.capacity()));
+
+    g.finish();
 }
 
 // Hash map cache, normal distribution.
 fn bench_hash_map_normal(c: &mut Criterion) {
-    let mut rng = rand::thread_rng();
-    let normal = Normal::new(0.0, 30.0).unwrap();
+    let mut g = c.benchmark_group("HashMap - Normal distribution");
+
     let mut cache = HashMap::new();
-    c.bench_function("HashMap (control, normal)", |b| {
+    let mut rng = ChaCha8Rng::seed_from_u64(RNG_SEED_VALUE);
+    let normal = Normal::new(0.0, NORMAL_VARIANCE_NARROW).unwrap();
+    g.bench_function("narrow range", |b| {
         b.iter_batched(
             || normal.sample(&mut rng) as i32,
             |input| {
@@ -52,34 +134,39 @@ fn bench_hash_map_normal(c: &mut Criterion) {
     });
 
     println!("Used cache size: {} bytes", cache_size(cache.capacity()));
-}
 
-// 128-element fixed-size cache, uniform distribution (fixed memory size: 128 * (32 + 32) bits = 1024 bytes).
-fn bench_memo_cache_uniform(c: &mut Criterion) {
-    let mut rng = rand::thread_rng();
-    let mut cache = MemoCache::<_, _, 128>::new();
-    c.bench_function("MemoCache (size: 128, uniform)", |b| {
+    let mut cache = HashMap::new();
+    let mut rng = ChaCha8Rng::seed_from_u64(RNG_SEED_VALUE);
+    let normal = Normal::new(0.0, NORMAL_VARIANCE_WIDE).unwrap();
+    g.bench_function("wide range", |b| {
         b.iter_batched(
-            || rng.gen_range(-100..=100),
+            || normal.sample(&mut rng) as i32,
             |input| {
-                cache.get_or_insert_with(&input, |_| {
+                if cache.get(&input).is_none() {
                     fake_expensive_calculation();
-                    42
-                });
+                    cache.insert(input, 42);
+                }
             },
             criterion::BatchSize::SmallInput,
         )
     });
 
     println!("Used cache size: {} bytes", cache_size(cache.capacity()));
+
+    g.finish();
 }
 
-// 128-element fixed-size cache, normal distribution (fixed memory size: 128 * (32 + 32) bits = 1024 bytes).
+// Fixed-size cache, normal distribution.
 fn bench_memo_cache_normal(c: &mut Criterion) {
-    let mut rng = rand::thread_rng();
-    let normal = Normal::new(0.0, 30.0).unwrap();
-    let mut cache = MemoCache::<_, _, 128>::new();
-    c.bench_function("MemoCache (size: 128, normal)", |b| {
+    let mut g = c.benchmark_group(format!(
+        "MemoCache (size: {}) - Normal distribution",
+        MEMO_CACHE_CAPACITY
+    ));
+
+    let mut cache = MemoCache::<_, _, MEMO_CACHE_CAPACITY>::new();
+    let mut rng = ChaCha8Rng::seed_from_u64(RNG_SEED_VALUE);
+    let normal = Normal::new(0.0, NORMAL_VARIANCE_NARROW).unwrap();
+    g.bench_function("narrow range", |b| {
         b.iter_batched(
             || normal.sample(&mut rng) as i32,
             |input| {
@@ -93,13 +180,33 @@ fn bench_memo_cache_normal(c: &mut Criterion) {
     });
 
     println!("Used cache size: {} bytes", cache_size(cache.capacity()));
+
+    let mut cache = MemoCache::<_, _, MEMO_CACHE_CAPACITY>::new();
+    let mut rng = ChaCha8Rng::seed_from_u64(RNG_SEED_VALUE);
+    let normal = Normal::new(0.0, NORMAL_VARIANCE_WIDE).unwrap();
+    g.bench_function("wide range", |b| {
+        b.iter_batched(
+            || normal.sample(&mut rng) as i32,
+            |input| {
+                cache.get_or_insert_with(&input, |_| {
+                    fake_expensive_calculation();
+                    42
+                });
+            },
+            criterion::BatchSize::SmallInput,
+        )
+    });
+
+    println!("Used cache size: {} bytes", cache_size(cache.capacity()));
+
+    g.finish();
 }
 
 criterion_group!(
     benches,
     bench_hash_map_uniform,
-    bench_hash_map_normal,
     bench_memo_cache_uniform,
-    bench_memo_cache_normal,
+    bench_hash_map_normal,
+    bench_memo_cache_normal
 );
 criterion_main!(benches);
